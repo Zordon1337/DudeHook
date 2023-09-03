7a183fa86968a8189482a36d044fb2fa1082c513
ZRD <ZRD@DESKTOP-2S08M61>
2023-09-03 11:37:04 +0200

    Now using buttons for esp instead of keybinds

--- DudeHook/Hack.cs
+++ DudeHook/Hack.cs
@@ -71,26 +71,36 @@ namespace DudeHook
             {
                 FindObjectOfType<PlayerParams>().GetMoney(1000);
             }
-            GUI.Label(new Rect(0, 150, 125, 25), "Kill all: " + KillAll);
+            if (GUI.Button(new Rect(0,150,125,25),"Kill everyone"))
+            {
+                foreach (var p in PeopleVar)
+                {
+                    p.HitMe(20000, false, false, p.gameObject);
+                }
+            }
+            if (GUI.Button(new Rect(0, 175, 125, 25), "Snaplines: "+Tracers))
+            {
+                Tracers = !Tracers;
+            }
+            if (GUI.Button(new Rect(0, 200, 125, 25), "Name ESP: "+NameESP))
+            {
+                NameESP = !NameESP;
+            }
+
+
+            
             foreach (var p in PeopleVar)
             {
                 Vector3 w2s = cam.WorldToScreenPoint(p.transform.position);
-                
-                
-                if(Tracers)
+
+
+                if (Tracers)
                 {
                     Render.DrawLine(new Vector2(Screen.width / 2, Screen.height / 2), new Vector2(w2s.x, Screen.height - w2s.y), Color.magenta, 5f);
                 }
-                if(NameESP)
+                if (NameESP)
                 {
                     GUI.Label(new Rect((int)w2s.x - 5, Screen.height - w2s.y - 14, 40, 40), "NPC");
-                }
-                if (KillAll)
-                    
-                {
-                    p.HitMe(20000,false,false,p.gameObject);
-
-
                 }
             }
 
@@ -98,19 +108,7 @@ namespace DudeHook
         
         public void Update()
         {
-            if(Input.GetKey(KeyCode.C))
-            {
-                Tracers = !Tracers;
-
-            }
-            if(Input.GetKey(KeyCode.V))
-            {
-                NameESP = !NameESP;
-            }
-            if(Input.GetKeyDown(KeyCode.Mouse4))
-            {
-                KillAll = !KillAll;
-            }
+            
             
         }
 
